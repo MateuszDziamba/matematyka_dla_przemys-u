282034174756fb5910cc69b11b0bc3fa3a54cd9a
Mateusz Dziamba <276221@student.pwr.edu.pl>
2025-03-18 16:50:20 +0100

    zmienony sposób rozmieszczania agentów na planszy

--- main.py
+++ main.py
@@ -2,33 +2,48 @@ import network
 import functions
 import random
 import numpy as np
+from seaborn import heatmap
+import matplotlib.pyplot as plt
 
-num_of_rows = 11
-num_of_columns = 11
+num_of_rows = 7
+num_of_columns = 7
 p0,p1,p2,p3,p4,p5 = 0.05,0.1,0.2,0.35,0.2,0.1 #wartości z artykułu
 
-net = network.network(num_of_rows,num_of_columns)
-net.add_exit(5,10,True)
-net.add_exit(6,10,True) #dodanie drugiego wyjścia
-net.add_exit(7,0,False) #dodanie trzeciego wyjścia
+net = network.network(num_of_rows,num_of_columns) #stworzenie sieci
+
+#dodanie wyjść na obu krawędziach
+for i in range(num_of_rows):
+    net.add_exit(i,0,False)
+    net.add_exit(i,num_of_columns-1,True)
 
 #rozmieszczenie agentów na planszy
-num_of_agents = 10
+num_of_agents = 30
 agents_positions = []
 for i in range(num_of_agents):
-    row = random.randint(0,num_of_rows-1)
-    column = random.randint(0,num_of_columns-1)
-    going_right = random.choice([True,False])
-    net.add_agent(row,column,going_right) #True - idzie w prawo, False - idzie w lewo
+    going_right = random.choice([True,False]) #True - idzie w prawo, False - idzie w lewo
     
+    #losowanie pozycji agentów jedynie na krawiędziach
+    if going_right:
+        row = random.randint(0,num_of_rows-1)
+        column = 0
+    else:
+        row = random.randint(0,num_of_rows-1)
+        column = num_of_columns-1 
+    net.add_agent(row,column,going_right) #dodaie agenta do sieci
+    
+    #zapisanie pozycji agentów w liście
     index = (row,column,going_right)
     if not index in agents_positions:
         agents_positions.append(index)
     
 #symulacja ruchu agentów
 while len(agents_positions) > 0:
-    new_agents_positions = []
+    print(net.matrix[::, ::, 0] + net.matrix[::, ::, 1])
+    #heatmap(net.matrix[::, ::, 0] + net.matrix[::, ::, 1], annot=True)
+    #plt.show()
+    #print(agents_positions)
     
+    new_agents_positions = []
     indexes_to_remove = []
     for index in agents_positions:
         if index in net.exits:
@@ -38,6 +53,7 @@ while len(agents_positions) > 0:
     for index in indexes_to_remove:
         agents_positions.remove(index)
         
+    Possible_collision = [] #przechowuje krotki postaci (r1,c1,r2,c2), gdzie (r1,c1) i (r2,c2) to kolejno pozycja przed i po (potencjalnym) ruchu   
     for index in agents_positions:
         row = index[0]
         column = index[1]
@@ -63,8 +79,7 @@ while len(agents_positions) > 0:
                 new_agents_positions.append((new_row,new_col,False))
             
     agents_positions = new_agents_positions
-    print(net.matrix[::, ::, 0])
-    #print(agents_positions)
+    
     
         
 
